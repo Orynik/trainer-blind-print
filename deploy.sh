46ea10bff6fc3685cb8e9a6dd7a6ddbcf56c2a62
Orynik <dkrovel@gmail.com>
2023-05-25 23:17:35 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com: main:gh-pages Orynik/trainer-blind-print.git
+git subtree push --prefix dist origin gh-pages
 
 cd -
